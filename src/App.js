65bf7ba980c040c205740f6a48e19ba09bea260f
Alan Desktop <albertodro0909@gmail.com>
2023-01-18 11:08:23 -0600

    Styles were added with Sass

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react'
-import FreeCodeCampLogo from './components/icons/FreeCodeCampLogo';
 import TodoAdd from './components/TodoAdd';
 import TodoList from './components/TodoList';
 import TodoItem from './components/TodoItem';
+import FreeCodeCampLogo from './components/icons/FreeCodeCampLogo';
 
 function App() {
 
-  const todos = [{text:"Buying black Halls"}];
+  const todos = [
+    {text: "Buying black Halls"},
+    {text: "Create a react app"},
+    {text: "Listening a podcast from mamadores experts on spotify"}
+  ];
 
   return (
     <div className="App">
-      <FreeCodeCampLogo/>
-      <div className="TodoContainer">
-        <h1>My todo's</h1>
-        <TodoAdd/>
-        <TodoList>
-          {todos.map( todo => 
-            <TodoItem 
-              key={todo.text} 
-              text={todo.text}/> 
-            )}
-        </TodoList>
+      <div className='TodoApp'>
+        <FreeCodeCampLogo/>
+        <div className="TodoContainer">
+          <h1 className='TodoContainer__title'>My todo's</h1>
+          <TodoAdd/>
+          <TodoList>
+            {todos.map( todo =>
+              <TodoItem
+                key={todo.text}
+                text={todo.text}/>
+              )}
+          </TodoList>
+        </div>
       </div>
     </div>
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 function TodoList(props) {
   return (
-    <section className='TodoList'>
-      <ul className='TodoList__list'>
-        {props.children}
-      </ul>
-    </section>
+    <ul className='TodoList'>
+      {props.children}
+    </ul>
   )
 }
 
